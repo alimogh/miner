932ea5f1194a00389b8726297509143e9753b48c
mambas2008 <64988464+mambas2008@users.noreply.github.com>
2020-05-12 03:04:41 +0700

    Create epic.sh

---
+++ epic.sh
@@ -0,0 +1,9 @@
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+
+./SRBMiner-MULTI --config-file ./Config/config-randomx.txt --pools-file ./Pools/randomx.txt
