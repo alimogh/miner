13ef35e4a363c576a903d73d534c131049dd2779
mambas2008 <64988464+mambas2008@users.noreply.github.com>
2020-05-11 17:21:46 +0700

    Delete start_cpuchain.bat

--- start_cpuchain.bat
+++
@@ -1,5 +0,0 @@
-@echo off	
-cd %~dp0	
-	
-cls	
-SRBMiner-MULTI.exe --algorithm cpupower --pool hashpex.com:3032 --wallet D7zknTTg7fY72FhRrQ6N9NigtcsWJ8FTux --password x --disable-gpu --log-file Logs\log-cpuchain.txt
