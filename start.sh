9edbf7da29d7d578e09870a12b0228ef831db9f4
mambas2008 <64988464+mambas2008@users.noreply.github.com>
2020-05-22 15:42:28 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ export GPU_MAX_SINGLE_ALLOC_PERCENT=100
 export GPU_ENABLE_LARGE_ALLOCATION=100
 export GPU_MAX_WORKGROUP_SIZE=1024
 
-./SRBMiner-MULTI --config-file ./Config/yespower.txt --pools-file ./Pools/multi.txt
+./SRBMiner-MULTI --config-file ./Config/config-yespower2b.txt --pools-file ./Pools/pool.txt
